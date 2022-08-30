e829b27302ef7d0f80c5c3bf6c1d6c8c6327b0f0
xiaogeminghai <xu_zhh@qq.com>
2022-08-30 15:09:44 +0800

    创建版本

---
+++ vofa_plus.c
@@ -0,0 +1,102 @@
+#include <vofa_plus.h>
+#include <rtthread.h>               
+#include <rtdevice.h>               
+#include <board.h> 
+#include "stdarg.h"
+
+
+#if PKG_USING_VOFA_PLUS
+
+
+#ifdef BSP_USING_UART4
+static rt_device_t uart4;
+static struct serial_configure uart4_config = RT_SERIAL_CONFIG_DEFAULT; 
+#endif
+
+
+
+char vofa_plus_transmit(vofa_plus_dev_struct_t_p dev,unsigned int number_of_channels,...)
+{
+    unsigned char idx;
+    unsigned int val;
+    va_list args;
+    va_start(args, number_of_channels);
+
+    if (number_of_channels > VOFA_PLUS_NUMBER_OF_CHANNELS)
+    {
+        va_end(args);
+        return -1;
+    }
+
+
+    for (idx = 0; idx < number_of_channels; ++idx) 
+    {
+
+        val = va_arg(args, unsigned int);
+        dev->tx_con_buff[idx] = val;
+    }
+    va_end(args);
+    memcpy(dev->tx_buff, (unsigned char *)(dev->tx_con_buff), sizeof(dev->tx_con_buff));
+    dev->tx_buff[VOFA_PLUS_NUMBER_OF_CHANNELS*4] = 0x00;                   
+    dev->tx_buff[VOFA_PLUS_NUMBER_OF_CHANNELS*4+1] = 0x00;
+    dev->tx_buff[VOFA_PLUS_NUMBER_OF_CHANNELS*4+2] = 0x80;
+    dev->tx_buff[VOFA_PLUS_NUMBER_OF_CHANNELS*4+3] = 0x7f;
+	#if defined( VOFA_PLUS_USING_UART1)
+		#ifdef BSP_USING_UART1
+		#endif
+	#elif defined( VOFA_PLUS_USING_UART4)
+		#ifdef BSP_USING_UART4
+		rt_device_write(uart4, 0, dev->tx_buff, VOFA_PLUS_BUFSZ);
+		#endif
+	#endif
+
+    return 0;
+  
+
+}
+
+
+
+
+
+void vofa_plus_init(void)
+{
+	rt_err_t ret = RT_EOK;
+	#ifdef BSP_USING_UART4
+    uart4 = rt_device_find("uart4");
+    if (!uart4)
+    {
+        rt_kprintf("find %s failed!\n", "uart4");
+        return RT_ERROR;
+    }
+    uart4_config.baud_rate = BAUD_RATE_115200;            
+    uart4_config.data_bits = DATA_BITS_8;                 
+    uart4_config.parity = PARITY_NONE;                 
+    uart4_config.stop_bits = STOP_BITS_1;                 
+    uart4_config.bit_order = BIT_ORDER_LSB;              
+    uart4_config.invert = NRZ_NORMAL;                 
+    uart4_config.flowcontrol = RT_SERIAL_FLOWCONTROL_NONE;  
+    uart4_config.bufsz = 128;                         
+    uart4_config.reserved = 0;                          
+    if (RT_EOK != rt_device_control(uart4, RT_DEVICE_CTRL_CONFIG, &uart4_config))
+    {
+        rt_kprintf("%s configuration error!\n", "uart4");
+        return RT_ERROR;
+    }
+    rt_device_open(uart4, RT_DEVICE_FLAG_INT_TX);
+	#endif
+    return ret;
+}
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
---
+++ vofa_plus.h
@@ -0,0 +1,62 @@
+/******************************************************************************
+
+                  版权所有 (C), 2018-2099, 小哥名海
+
+ ******************************************************************************
+  文 件 名   : vofa_plus.h
+  版 本 号   : 初稿
+  作    者   : sgeeood
+  生成日期   : 2022年8月22日
+  最近修改   :
+  功能描述   : vofa_plus.c 的头文件
+  
+  修改历史   :
+  1.日    期   : 2022年8月22日
+    作    者   : sgeeood
+    修改内容   : 创建文件
+
+******************************************************************************/
+
+#ifndef __VOFA_PLUS_H__
+#define __VOFA_PLUS_H__
+
+
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C"{
+#endif
+#endif /* __cplusplus */
+
+
+#if PKG_USING_VOFA_PLUS
+
+#define VOFA_PLUS_NUMBER_OF_CHANNELS 1
+#define VOFA_PLUS_UNIT_BYTES 4
+#define VOFA_PLUS_BUFSZ ((VOFA_PLUS_NUMBER_OF_CHANNELS+1)*VOFA_PLUS_UNIT_BYTES)
+
+
+struct vofa_plus_dev_s
+{
+    //const char *name;
+	unsigned char tx_buff[VOFA_PLUS_BUFSZ];
+	float tx_con_buff[VOFA_PLUS_NUMBER_OF_CHANNELS];
+
+};
+typedef struct vofa_plus_dev_s vofa_plus_dev_struct_t;
+typedef struct vofa_plus_dev_s * vofa_plus_dev_struct_t_p;
+
+
+extern char vofa_plus_transmit(vofa_plus_dev_struct_t_p dev,unsigned int number_of_channels,...);
+extern void vofa_plus_init(void);
+
+#endif
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif /* __cplusplus */
+
+
+#endif /* __VOFA_PLUS_H__ */
